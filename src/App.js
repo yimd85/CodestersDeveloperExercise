f3730f738130d13ad11dd87915aaa5855dc4de78
David Yim <yimd85@gmail.com>
2018-10-19 23:14:14 -0400

    completed math portion

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint no-eval: 0 */
 import React, { Component } from 'react';
 
 import Operator from './components/Operator.js';
@@ -6,61 +7,75 @@ import './App.css';
 
 class App extends Component {
   state = {
-    test: [ {b1: false, op2: "|", b2: true }]
+    array: [ {parenOne: "(", bineryOne: true, secondOperator: "&", bineryTwo: false, parenTwo: ")" }],
+    finalAnswer: 0
   };
 
-  addRow = () => {
-    let piece = this.state.test;
-    piece.push({op1: "^", b1: false, op2: "|", b2: true })
-    this.setState ({ test: piece }
-    ,
-      () => { console.log(this.state.test); }
-    );
+  addRow() {
+    let copy = this.state.array;
+    copy.push({parenOne: "(", firstOperator: "&", bineryOne: true, secondOperator: "&", bineryTwo: false, parenTwo: ")" })
+    this.setState ({ array: copy }, () => { this.calculate(); });
   }
 
-  removeRow = () => {
-    let copy = this.state.test;
-    if( copy.length > 1) {
-      copy.pop();
+  removeRow() {
+    let copy = this.state.array;
+    if( copy.length > 1) { copy.pop(); }
+    this.setState ({ array: copy }, () => { this.calculate(); });
+  }
+
+  operatorChange1 (index, event) {
+    let copy = this.state.array;
+    copy[index].firstOperator = event.target.value;
+    this.setState ({ array: copy }, () => { this.calculate(); });
+  }
+
+  operatorChange2 (index, event) {
+      let copy = this.state.array;
+      copy[index].secondOperator = event.target.value;
+    this.setState ({ array: copy }, () => { this.calculate(); });
+  }
+
+  changebineryOne (event, index){
+    let x;
+    if( event === true ) { x = false; } else { x = true; }
+    let copy = this.state.array;
+    copy[index].bineryOne = x;
+    this.setState ({ array: copy }, () => { this.calculate(); });
+  }
+
+  changebineryTwo (event, index){
+    let x;
+    if( event === true ) { x = false; } else { x = true; }
+    let copy = this.state.array;
+    copy[index].bineryTwo = x;
+    this.setState ({ array: copy }, () => { this.calculate(); });
+  }
+
+  calculate() {
+    let copy = this.state.array;
+    var vals=[];
+    for(let i=0; i< copy.length; i++){
+      if(i===0){
+        vals.push(copy[i].parenOne, copy[i].bineryOne, copy[i].secondOperator, copy[i].bineryTwo, copy[i].parenTwo);
+      } else {
+        vals.push( copy[i].firstOperator, copy[i].parenOne, copy[i].bineryOne, copy[i].secondOperator, copy[i].bineryTwo, copy[i].parenTwo);
+      }
     }
-    this.setState ({ test: copy }
-      ,
-        () => { console.log(this.state.test); }
-    );
+    let x = eval(vals.join(''));
+    this.setState({ finalAnswer: x });
   }
 
-  operatorChange = (event) => {
-    console.log(event);
-    this.setState({ value: event.target.value})
-  }
-
-  changeB1 (event, index){
-    let x;
-    if(event === true) { x = false; } else { x = true; }
-    let copy = this.state.test;
-    copy[index].b1 = x;
-    this.setState({ test: copy})
-  }
-
-  changeB2 (event, index){
-    let x;
-    if(event === true) { x = false; } else { x = true; }
-    let copy = this.state.test;
-    copy[index].b2 = x;
-    this.setState({ test: copy})
-  }
-
-  renderHelperFunction() {
-    return this.state.test.map((item, index) => {
+  renderSwitches() {
+    return this.state.array.map((item, index) => {
       if( index ===  0 ){
        return (
          <span key={index}>
                  <div>
-                       <Buttons className={item.b1 ? 'true' : 'false'} indication={item.b1 ? 'On' : 'Off'} onClick={() => this.changeB1(item.b1, index)}/>
-                       <Operator value={item.op2} onChange={this.operatorChange} />
-                       <Buttons className={item.b2  ? 'true' : 'false'} indication={item.b2 ? 'On' : 'Off'} onClick={() => this.changeB2(item.b2, index)}/>
-                       <Buttons className={"add"} indication={"+"} onClick={this.addRow}/>
-                       <Buttons className={"remove"} indication={"-"} onClick={this.removeRow} />
+                       <Buttons className={item.bineryOne ? 'true' : 'false'} indication={item.bineryOne ? 'On' : 'Off'} onClick={() => this.changebineryOne(item.bineryOne, index)}/>
+                       <Operator value={item.secondOperator} onChange={(event) => this.operatorChange2(index, event)} />
+                       <Buttons className={item.bineryTwo  ? 'true' : 'false'} indication={item.bineryTwo ? 'On' : 'Off'} onClick={() => this.changebineryTwo(item.bineryTwo, index)}/>
+                       <Buttons className={"add"} indication={"+"} onClick={() => this.addRow()}/>
+                       <Buttons className={"remove"} indication={"-"} onClick={() => this.removeRow()} />
                  </div>
          </span>
        );
@@ -68,13 +83,12 @@ class App extends Component {
        return (
          <span key={index}>
                  <div className="container row">
-                   <Operator value={item.op1} onChange={this.operatorChange}/>
+                   <Operator value={item.firstOperator} onChange={(event) => this.operatorChange1(index, event)}/>
                  </div>
-
                  <div>
-                       <Buttons className={item.b1 ? 'true' : 'false'} indication={item.b1 ? 'On' : 'Off'} onClick={() => this.changeB1(item.b1, index)}/>
-                       <Operator value={item.op2} onChange={this.operatorChange} />
-                       <Buttons className={item.b2  ? 'true' : 'false'} indication={item.b2 ? 'On' : 'Off'} onClick={() => this.changeB2(item.b2, index)}/>
+                       <Buttons className={item.bineryOne ? 'true' : 'false'} indication={item.bineryOne ? 'On' : 'Off'} onClick={() => this.changebineryOne(item.bineryOne, index)}/>
+                       <Operator value={item.secondOperator} onChange={(event) => this.operatorChange2(index, event)} />
+                       <Buttons className={item.bineryTwo  ? 'true' : 'false'} indication={item.bineryTwo ? 'On' : 'Off'} onClick={() => this.changebineryTwo(item.bineryTwo, index)}/>
                  </div>
          </span>
        );
@@ -84,99 +98,12 @@ class App extends Component {
 
 	render() {
     return (
-      <div className="container row">
-                  {this.renderHelperFunction() }
-                  <div className="light true"><span>OFF</span></div>
+        <div className="container row">
+                  {this.renderSwitches() }
+                  <div className={this.state.finalAnswer  ? 'light true' : 'light false'}><span>{this.state.finalAnswer  ? 'On' : 'Off'}</span></div>
         </div>
     );
   }
 }
 
 export default App;
-
-
-//
-// function init_jquery(){
-//   var $root = $('#jquery-root');
-//   var $light = $('.light');
-//   var $mode = $('#mode');
-//
-//   function setOn($element){
-//     $element.data('state', 'on');
-//     $element.addClass('on');
-//     $element.removeClass('off');
-//     $element.html("<span>ON</span>");
-//   }
-//
-//   function setOff($element){
-//     $element.data('state', 'off');
-//     $element.addClass('off');
-//     $element.removeClass('on');
-//     $element.html("<span>OFF</span>");
-//   }
-//
-//   function switchLogic(s1, s2){
-//     switch($mode.val()){
-//       case "or":
-//         return s1 || s2;
-//       case "xor":
-//         return (s1 || s2) && !(s1 && s2);
-//       case "and":
-//         return s1 && s2;
-//       default:
-//         return false;
-//     }
-//   }
-//
-//   function updateLight(){
-//     var switches = $('.switch')
-//     var sw1 = switches[0];
-//     var sw2 = switches[1];
-//
-//     var state1 = $(sw1).data('state') === 'on';
-//     var state2 = $(sw2).data('state') === 'on';
-//     if (switchLogic(state1, state2)){
-//       setOn($light);
-//     }
-//     else{
-//       setOff($light);
-//     }
-//   }
-//
-//   $('button.switch').click(function(){
-//     var $btn = $(this);
-//     if ($btn.data('state') === 'on'){
-//       setOff($btn);
-//     }
-//     else if ($btn.data('state') === 'off'){
-//       setOn($btn);
-//     }
-//     updateLight();
-//   })
-//
-//   $('select#mode').change(function(){
-//     updateLight();
-//   })
-//
-// }
-//
-// $(init_jquery);
-
-
-// Plans
-// console.log((true^false)&(true^false)&(true^false));
-//
-//
-//
-// const x = ['true', '^', 'false', '&','true', '^', 'false','&','true', '^', 'false', '&','true', '^', 'false'];
-//
-// console.log(eval(x.join('')));
-// line (add a line)
-// operator
-// line (remove a line)
-//  operator
-// line (remove a line)
-
-// push operator, a button, operator and button.
-// use ternary operatorrs for true and false.
-// use case for operators
